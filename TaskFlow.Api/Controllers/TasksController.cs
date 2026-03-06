e679ad0a0f5a8ee64d634aba9352271ecce2c644
Ayoub ESSARGHINI <a.essarghiniy@gmail.com>
2026-03-06 15:02:13 +0000

    feat(api): add UpdateTaskDto and PUT endpoint to TasksController

--- TaskFlow.Api/Controllers/TasksController.cs
+++ TaskFlow.Api/Controllers/TasksController.cs
@@ -3,6 +3,7 @@ using TaskFlow.Application.DTOs;
 using TaskFlow.Application.Interfaces;
 using TaskFlow.Domain.Entities;
 using TaskFlow.Infrastructure.Data;
+using TaskStatus = TaskFlow.Domain.Enums.TaskStatus;
 
 namespace TaskFlow.Api.Controllers;
 
@@ -38,9 +39,56 @@ public class TasksController : ControllerBase
   }
 
   [HttpGet("{id}")]
-  public async Task<ActionResult<IEnumerable<TaskItem>>> GetTask(string id)
+  public async Task<ActionResult<TaskResponseDto>> GetTaskById(Guid id)
   {
-    var task = await _taskRepository.GetByIdAsync(Guid.Parse(id));
-    return Ok(task);
+    var task = await _taskRepository.GetByIdAsync(id);
+    
+    if (task == null)
+    {
+      return NotFound(new { message = "Task not found." }); 
+    }
+
+    var response = new TaskResponseDto
+    {
+      Id = task.Id,
+      Title = task.Title,
+      Description = task.Description,
+      Status = task.Status.ToString(),
+    };
+
+ 
+    return Ok(response);
+  }
+
+  [HttpPut("{id}")]
+  public async Task<ActionResult> UpdateTaskById([FromBody] CreateTaskDto dto, Guid id)
+  {
+    var task = _taskRepository.GetByIdAsync(id);
+    if (task == null)
+    {
+      return NotFound(new {message = "Not found."});
+    }
+
+    if (!Enum.TryParse<TaskStatus>(dto.Status, true, out var status)) return BadRequest();
+    var newTask = new TaskItem
+    {
+      Title = dto.Title,
+      Description = dto.Description,
+      Status = status
+    };
+    await _taskRepository.UpdateAsync(newTask);
+    return CreatedAtAction(nameof(GetTaskById), new { id = task.Id }, task);
+  }
+
+  [HttpDelete("{id}")]
+  public async Task<ActionResult> DeleteTaskById(Guid id)
+  {
+    var task = _taskRepository.GetByIdAsync(id);
+    if (task == null)
+    {
+      return NotFound();
+    }
+    await _taskRepository.DeleteAsync(id);
+    return NoContent();
   }
 }
--- TaskFlow.Api/Program.cs
+++ TaskFlow.Api/Program.cs
@@ -6,11 +6,13 @@ using TaskFlow.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
-
+builder.Services.AddSwaggerGen();
 // postgreSql Connection
 var connectionString = builder.Configuration.GetConnectionString("TasksDb");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
@@ -18,12 +20,33 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql
 //Register Dependency Injection
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 
+
 var app = builder.Build();
+
+
+// This creates a temporary scope to grab our database context and run migrations
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        // This command checks for pending migrations and applies them automatically!
+        context.Database.Migrate(); 
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();   // Add this!
+    app.UseSwaggerUI(); // Add this!
 }
 
 // app.UseHttpsRedirection();
