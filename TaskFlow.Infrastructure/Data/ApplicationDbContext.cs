25c5b7b361b44eab7b1b3550a5eb001bca450929
Ayoub ESSARGHINI <a.essarghiniy@gmail.com>
2026-03-06 14:28:46 +0000

    chore(database): generate InitialCreate EF Core migrations

--- TaskFlow.Infrastructure/Data/ApplicationDbContext.cs
+++ TaskFlow.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,6 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Domain.Entities;
+
 namespace TaskFlow.Infrastructure.Data;
 
-public class ApplicationDbContext
+public class ApplicationDbContext : DbContext
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
     
+    public DbSet<TaskItem> Tasks { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+       base.OnModelCreating(modelBuilder);
+       
+       modelBuilder.Entity<TaskItem>().ToTable("tasks");
+       modelBuilder.Entity<TaskItem>().HasKey(x => x.Id);
+       modelBuilder.Entity<TaskItem>().Property(t => t.Title).IsRequired().HasMaxLength(200);
+    }
 }
--- TaskFlow.Infrastructure/Repositories/TaskRepository.cs
+++ TaskFlow.Infrastructure/Repositories/TaskRepository.cs
@@ -1,6 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Application.Interfaces;
+using TaskFlow.Domain.Entities;
+using TaskFlow.Infrastructure.Data;
+
 namespace TaskFlow.Infrastructure.Repositories;
 
-public class TaskRepository
+public class TaskRepository : ITaskRepository
 {
-    
+    private readonly ApplicationDbContext _db;
+
+    public TaskRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public async Task<IEnumerable<TaskItem>> GetAllAsync()
+    {
+        return await  _db.Tasks.ToListAsync();
+    }
+
+    public async Task<TaskItem?> GetByIdAsync(Guid id)
+    {
+        return await _db.Tasks.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task AddAsync(TaskItem task)
+    {
+        await _db.Tasks.AddAsync(task);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(TaskItem task)
+    {
+        _db.Tasks.Update(task);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+       var task = await _db.Tasks.FindAsync(id);
+       if (task != null)
+       {
+            _db.Tasks.Remove(task);
+            await _db.SaveChangesAsync();
+       }
+       
+    }
 }
